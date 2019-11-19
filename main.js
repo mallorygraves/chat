f150653900871a49354dfe82a72ea62e6d36dcb6
Mallory Graves <Mallory.Graves@sckans.edu>
2019-11-18 22:12:42 -0600

    chat

--- main.js
+++ main.js
@@ -1,33 +1,9 @@
-for(let i = 0; < 1024; i++){
-    document.querySelector(".app").innerHTML += '<div></div>';
-}
-document.body.addEventListener('click', event => {
-   event.target.style.backgroundColor = '#fff';
-   event.target.style.border = '1 px solid #000';
-})
-document.addEventListener('keydown', event => {
-    if(event.key === 'e'){
-        document.querySelector(".app").style.backgroundColor = '#30f';
-    }
-    if(event.key === 'i'){
-        document.querySelector(".app").style.backgroundColor = '#60f';
-    }
-    if(event.key === 'i'){
-        document.querySelector(".app").style.backgroundColor = '#90f';
-    }
-    if(event.key === 'i'){
-        document.querySelector(".app").style.backgroundColor = '#a0f';
-    }
-    if(event.key === 'i'){
-        document.querySelector(".app").style.backgroundColor = '#o0f'
-    }
-})
-document.querySelector('clear').addEventListener('click', event =>{
-   window.location = window.location;
-})
-document.body.addEventListener('click', event =>{
-  if(event.ctrlKey){
-      event.target.innerText = document.querySelector('#text').Value;
-      event.target.style.backgroundColor='rgb(0,'+ blue + ',0)';
-  }
-})
+const socket = new WebSocket('wss://www.el-evan.com/web_dev_chat_server');
+
+document.querySelector('#Submit Message').addEventListener('click', () => {
+    socket.send(JSON.stringify({Name : document.querySelector('#name').value, Message : document.querySelector('#message').value}))
+});
+
+socket.addEventListener('message', event => {
+    const message = JSON.parse(event.data);
+    document.querySelector('.chat-stream').innerText += message.Name + '::' + message.Message + '\n';
